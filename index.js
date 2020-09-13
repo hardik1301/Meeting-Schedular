bf9f30d53852989348d26ee895e4aed3e8681527
HARDIK KARIRA <52444464+hardik1301@users.noreply.github.com>
2020-09-13 23:58:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+
+
+ReactDOM.render(
+  <Counter/>,
+  document.getElementById('root')
+);
+
